9de0cddfe2457ff1050d1ad7f0b3ac7501efc2e0
nabby27 <ivancordoba77@gmail.com>
2022-04-29 22:13:59 +0200

    send click event message for persist

--- src/pages/Background/index.ts
+++ src/pages/Background/index.ts
@@ -1,4 +1,13 @@
 // This page is the background page for the extension. 
 // It is always running and should be used for any background tasks.
 
+import { RecordDataService } from "../../service/RecordData.service";
+
 console.log("Background page loaded, logic should be written here.");
+
+chrome.runtime.onMessage.addListener(async function (msg, sender) {
+    if (msg.type === "click") {
+        const currentDatetime = new Date().toISOString();
+        await RecordDataService.addNewData(currentDatetime, msg.data);
+    }
+});
--- src/pages/Content/index.ts
+++ src/pages/Content/index.ts
@@ -1,4 +1,18 @@
 // This script is injected into all pages used in chrome
 // Should be use to capture all events and send them to the background page
 
+import { StateService } from "../../service/State.service";
+
 console.log("Content page loaded, event capture logic should appear here.");
+
+window.addEventListener("click", async (e) => {
+    const isRecording = await StateService.getRecordingState()
+    if (isRecording) {
+        chrome.runtime.sendMessage({
+            type: "click",
+            data: {
+                text: (e.target as HTMLElement).innerText,
+            },
+        });
+    }
+});
