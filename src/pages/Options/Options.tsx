ee2cb6433c7434a8e79fa5934ebf9f34f164f47c
nabby27 <ivancordoba77@gmail.com>
2022-04-29 22:12:53 +0200

    add styles and logic to options

--- src/pages/Options/Options.tsx
+++ src/pages/Options/Options.tsx
@@ -1,12 +1,36 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { RecordDataInDatetime, RecordDataService } from '../../service/RecordData.service';
 import './Options.css';
+import { Records } from './Records/Records';
 
 interface Props {
   title: string;
 }
 
 export function Options({ title }: Props) {
-  return (<div className="OptionsContainer">
-    User clicks should be displayed here.
-  </div>);
+  const [data, setData] = useState<RecordDataInDatetime>();
+
+  
+  useEffect(() => {
+    async function loadData() {
+      const loadedData = await RecordDataService.getRecordDataInDatetime();
+      setData(loadedData);
+    }
+    
+    loadData()
+
+    chrome.storage.onChanged.addListener((allData) => {
+      setData(allData.uphint?.newValue);
+    })
+  }, []);
+
+  return (
+    <div className="OptionsContainer">
+      <h1 className="Title">
+        User clicks should be displayed here.
+      </h1>
+
+      {data && <Records records={data}/>}
+    </div>
+  );
 };
