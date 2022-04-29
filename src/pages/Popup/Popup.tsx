c5523ba40957a2927bd279371583a78fdf02a32b
nabby27 <ivancordoba77@gmail.com>
2022-04-29 22:11:43 +0200

    add styles and logic to popup

--- src/pages/Popup/Popup.tsx
+++ src/pages/Popup/Popup.tsx
@@ -1,11 +1,39 @@
-import React from 'react';
-import logo from '../../assets/img/logo.svg';
+import { useEffect, useState } from 'react';
+import { StateService } from '../../service/State.service';
+import { Logo } from './Logo/Logo';
 import './Popup.css';
+import { RecordingButton } from './RecordingButton/RecordingButton';
 
 export function Popup(){
+  const [isRecording, setIsRecording] = useState(false);
+
+  useEffect(() => {
+    const initializeState = async () => {
+      const isRecording = await StateService.getRecordingState()
+      setIsRecording(isRecording)
+    }
+
+    initializeState()
+  }, [])
+
   return (
     <div className="App">
-      Start Recording and recording status should be displayed here.
+
+      <Logo isRecording={isRecording}/>
+      
+      {isRecording &&
+        <h1 className="RecordingText">
+          Recording
+        </h1>
+      }
+
+      <RecordingButton
+        isRecording={isRecording}
+        onClick={() => {
+          setIsRecording(!isRecording)
+          StateService.changeRecordingState(!isRecording)
+        }}
+      />
     </div>
   );
 };
